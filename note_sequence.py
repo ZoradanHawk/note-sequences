8b9b595f6e47886ecd321429c8911aac55f5537a
ZoradanHawk <nanotg@libero.it>
2017-04-02 01:52:46 +0100

    New Version
    - Name changed to 'Sequences'.
    - New superclass Sequence, subclasses SparseSequence, NoteSequence and ChordSequence.
    - Subclasses inherit magic methods from Sequence.
    - NoteSequence and subclass ChordSequence now take a map file name as argument and call mapping_toolkit.read_map_file internally.  .txt maps are therefore no longer optional.

--- note_sequence.py
+++ note_sequence.py
@@ -1,90 +1,141 @@
-author = 'Thomas Grossi'
-version = '0.3'
-
 import random
+import sequence_toolkit as toolkit
+import debug_toolkit as debug
+import mapping_toolkit as mappings
 
-from midi_IO import input_midi_chords
-from transitions import TransitionMatrix
-from sections import Section, TransitionSection
 
+class Sequence(object):
 
-class NoteSequence(object):
-    def __init__(self, notes, structure, mapping, section, transition):
-        self.matrix = TransitionMatrix(numbers=notes)
-        self.total_length = ((section * len(structure)) +
-                            (transition * (len(structure) - 1)))
-        self.get_note = self.note_generator(initial_note=random.choice(notes),
-                                            length=self.total_length,
-                                            matrix=self.matrix)
-        self.data = self.create_sequence(structure, mapping, section,
-                                             transition)
+    @debug.sequence_init
+    def __init__(self, melody, length):
+        self.melody = melody
+        self.total_length = length
+        self.data = self.create_sequence()
 
     @staticmethod
-    def note_generator(initial_note, matrix, length):
+    def note_generator(melody, length):
         '''Starting from *initial_note*, generates *length*
         notes based on the choices and probabilities
         specified in *matrix*. '''
-        note = initial_note
+        note = random.choice(melody)
+        matrix = toolkit.create_transition_matrix(melody)
         for i in range(length):
+            if not matrix[note]:
+                note = max(matrix, key=lambda x: len(matrix[x]))
             rand = random.random()
-            for pos, num in enumerate(matrix[note].probs):
-                if rand < num:
-                    note = matrix[note].choices[pos]
+            for possible_note, probability in matrix[note]:
+                if rand < probability:
+                    note = possible_note
                     break
             yield note
 
-    def create_sequence(self, structure, mapping, section, transition):
-        '''Builds a sequence with section structure *structure*, where
-        conversion of notes or chords from one section to the next are
-        defined in *mapping*. Arguments *section* and *transition*
-        refer to the lengths (in notes) of normal and transition
-        sections within the sequence. Returns a list.'''
-        sequence = []
-        for i, letter in enumerate(structure):
-            section = Section(generator=self.get_note,
-                              length=section,
-                              mapping=mapping,
-                              section_letter=letter).create_section()
-            sequence.extend(section)
-            try:
-                next_letter = structure[i + 1]
-                ascend = letter < next_letter
-                section = TransitionSection(generator=self.get_note,
-                                            length=transition,
-                                            mapping=mapping,
-                                            first=letter,
-                                            second=next_letter,
-                                            ascend=ascend).transition_section()
-                sequence.extend(section)
-            except IndexError:
-                pass
-        return sequence
+    def create_sequence(self):
+        generator = self.note_generator(self.melody, self.total_length)
+        return [next(generator) for _ in range(self.total_length)]
 
     def __iter__(self):
         for note in self.data:
             yield note
 
+    def __str__(self):
+        return str(list(self.data))
+
+    def __len__(self):
+        return self.total_length
+
     def __getitem__(self, number):
         return self.data[number]
 
-    def __len__(self):
-        return len(self.data)
 
-    def __str__(self):
-        return str(self.data)
+class NoteSequence(Sequence):
 
-    def __repr__(self):
-        return str(self.data)
+    @debug.note_sequence_init
+    def __init__(self, melody, map_filename):
+        self.structure, self.map, self.sections,
+        self.transitions = mappings.read_map_file(map_filename)
+        Sequence.__init__(self, melody, sum(self.sections + self.transitions))
+
+    def create_sequence(self):
+        sequence = []
+        generator = Sequence.note_generator(self.melody, self.total_length)
+        section_lengths = (length for length in self.sections)
+        transition_lengths = (length for length in self.transitions)
+        for i, letter in enumerate(self.structure):
+            section = toolkit.Section(generator=generator,
+                                      length=next(section_lengths),
+                                      mapping=self.map,
+                                      section=letter)
+            for note in section:
+                sequence.append(note)
+            try:
+                next_section = self.structure[i + 1]
+                transition = toolkit.Transition(generator=generator,
+                                             length=next(transition_lengths),
+                                             mapping=self.map,
+                                             section=letter,
+                                             next_section=next_section)
+                for note in transition:
+                    sequence.append(note)
+            except IndexError:
+                pass
+        return sequence
 
 
-def main():
-    chords = input_midi_chords('chord_test.mid')
-    chord_mapping = {'A': list(set(chords)),
-               'B': [tuple(x + 7 for x in e) for e in list(set(chords))],
-               'C': [tuple(x + 12 for x in e) for e in list(set(chords))]}
-    seq = NoteSequence(chords, 'ABACA', chord_mapping, 200, 200)
-    print(seq)
+class SparseSequence(Sequence):
+    def __init__(self, melody, length, pause_value=61):
+        self.pause_value = pause_value
+        Sequence.__init__(self, melody, length)
+
+    def create_sequence(self):
+        sequence = []
+        generator = Sequence.note_generator(self.melody, self.total_length)
+        for i in range(self.total_length):
+            prob = i / float(self.total_length)
+            if random.random() < prob:
+                sequence.append(next(generator))
+            else:
+                sequence.append((self.pause_value,))
+        return sequence
 
 
-if __name__ == '__main__':
-    main()
+class ChordSequence(NoteSequence):
+    def __init__(self, melody, map_filename, chord_increase=1):
+        self.chord_increase = chord_increase
+        NoteSequence.__init__(self, melody, map_filename)
+
+    def _update(self, note, prob, note_set):
+        for _ in range(self.chord_increase):
+            if random.random() < prob:
+                note_set = [o for o in note_set if o[0] not in note]
+                note += random.choice(note_set) if note_set else ()
+        return note
+
+    def create_sequence(self):
+        sequence = []
+        generator = Sequence.note_generator(self.melody, self.total_length)
+        section_lengths = (length for length in self.sections)
+        transition_lengths = (length for length in self.transitions)
+        prob = 0.0
+        for i, letter in enumerate(self.structure):
+            note_set = self.map[letter]
+            section = toolkit.Section(generator=generator,
+                                      length=next(section_lengths),
+                                      mapping=self.map,
+                                      section=letter)
+            for note in section:
+                sequence.append(self._update(note, prob, note_set))
+                prob += 1 / float(self.total_length)
+            try:
+                next_section = self.structure[i + 1]
+                note_set = self.map[letter] + self.map[self.structure[i + 1]]
+                transition = toolkit.Transition(generator=generator,
+                                             length=next(transition_lengths),
+                                             mapping=self.map,
+                                             section=letter,
+                                             next_section=next_section)
+                for note in transition:
+                    sequence.append(self._update(note, prob, note_set))
+                    prob += 1 / float(self.total_length)
+            except IndexError:
+                pass
+        return sequence
