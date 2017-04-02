fbd65aa3e1e5a2f64540cbbdf1588f197cd5c54c
ZoradanHawk <nanotg@libero.it>
2017-04-03 00:00:20 +0100

    Corrected bug in arguments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ notes = midi.midi_input('test_input.mid')
 # Example of creating sequences
 seq = sequences.Sequence(melody=notes, length=100)
 note_seq = sequences.NoteSequence(melody=notes, map_filename='test_map.txt')
-sparse_seq = sequences.SparseSequence(melody=notes, length=100, pause_note=60)
+sparse_seq = sequences.SparseSequence(melody=notes, length=100, pause_value=60)
 chord_seq = sequences.ChordSequence(melody=notes, map_filename='test_map.txt', chord_increase=3)
 
 midi.midi_output(filename='test_output_with_rhythms.mid', sequence=seq, rhythms=[(2, 3), (2, 2)])
