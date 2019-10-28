0039d93c3b6a0dfff5b6aeb02b79b22205b32d55
ZoradanHawk <24376690+ZoradanHawk@users.noreply.github.com>
2019-10-28 18:54:07 +0100

    Checking code version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ __version__ = '0.85'
 
 import os
 
-from midi_toolkit import read_melody, write_midifile
+from midi_toolkit import read_melody, read_rhythms, write_midifile
 from sequences import create_mapseq, create_sparseseq, create_chordseq
 import sequence_toolkit as tools
 
@@ -18,31 +18,44 @@ def get_map():
             raise SystemExit('Invalid File Name. No .txt files detected in your directory.')
         print('Invalid File Name. .txt files in your directory: {}.'.format(
                                                                         maps))
-        user_input = str(input('Try again? Y|N '))
+        user_input = str(input('Try again? Y|N: '))
         if user_input.upper() == 'Y':
             map_file = get_map()
         else:
             raise SystemExit()
     return map_file
 
-def get_melody():
+
+def get_input_method():
+    which_method = str(input('Do you want to extract the melody or the ' + 
+                               'rhythms from the sequence? (1|2): '))
+    if which_method not in ['1', '2']:
+        user_input = str(input('Incorrect Option. Try again? (Y|N): '))
+        if user_input.upper() == 'Y':
+            which_method = get_input_method()
+        else:
+            raise SystemExit()
+    return read_melody if which_method == '1' else read_rhythms
+
+
+def get_input_sequence(function):
     midifile = str(input('Input File Name (midi): '))
     try:
-        melody = read_melody(midifile)
+        input_sequence = function(midifile)
     except (FileNotFoundError, OSError):
         midi_files = [f for f in next(os.walk('./'))[2] if f[-4:] == '.mid']
         if not midi_files:
             raise SystemExit('Invalid File Name. No midi files detected in your directory.')
         print('Invalid File Name. Midi Files in your directory: {}.'.format(
                                                                 midi_files))
-        user_input = str(input('Try again? Y|N '))
+        user_input = str(input('Try again? Y|N: '))
         if user_input.upper() == 'Y':
-            melody = get_melody()
+            input_sequence = get_input_sequence(function)
         else:
             raise SystemExit()
-    if not melody:
+    if not input_sequence:
         raise SystemExit('Problem with the midi file. Try another one.')
-    return melody
+    return input_sequence
 
 
 def get_output_name():
@@ -52,7 +65,7 @@ def get_output_name():
             return '{}.mid'.format(output_name)
         else:
             print('Invalid file name. Output file must have a .mid extension.')
-            user_input = str(input('Try again? Y|N '))
+            user_input = str(input('Try again? Y|N: '))
             if user_input.upper() == 'Y':
                 output_name = get_output_name()
             else:
@@ -60,9 +73,35 @@ def get_output_name():
     return output_name
 
 
+def get_length():
+    length = input('Sequence length (in notes): ')
+    try:
+        return int(length)
+    except ValueError:
+        user_input = str(input('Length must be an integer. Try again? Y|N: '))
+        if user_input.upper() == 'Y':
+            length = get_length()
+        else:
+            raise SystemExit()
+    return int(length)
+
+
+def get_segment_size():
+    size = input('Segment size: ')
+    try:
+        return int(size)
+    except ValueError:
+        user_input = str(input('Size must be an integer. Try again? Y|N: '))
+        if user_input.upper() == 'Y':
+            size = get_segment_size()
+        else:
+            raise SystemExit()
+    return int(length)
+
 def write_seq(melody):
-    length = str(input('Sequence length (in notes): '))
-    return list(tools.generate_sequence(melody, int(length)))
+    length = get_length()
+    return list(tools.generate_sequence(melody, length))
+
 
 
 def write_mapseq(melody):
@@ -71,7 +110,7 @@ def write_mapseq(melody):
 
 
 def write_sparseseq(melody):
-    length = str(input('Sequence length (in notes): '))
+    length = get_length()
     from_user = str(input('''Do you want the sequence to emerge from silence 
                       or to fade away? 1|2: '''))
     fading = False if from_user == '1' else True
@@ -85,42 +124,50 @@ def write_chordseq(melody):
 
 
 def write_groupseq(melody):
-    grouping = str(input('Pauses or Pitch? 1|2: '))
+    grouping = str(input('Do you want to group by pauses, pitch or segments ' +
+                         'of a specified size? 1|2|3: '))
     if grouping == '1':
         seq = tools.group_by_pauses(melody)
     elif grouping == '2':
         seq = tools.group_by_pitch(melody)
+    elif grouping == '3':
+        segment_size = get_segment_size()
+        seq = tools.group_by_segment_size(melody, segment_size)
     else:
-        from_user = str(input('Incorrect grouping method. Try again? Y|N '))
+        from_user = str(input('Incorrect grouping method. Try again? Y|N: '))
         if from_user == 'Y':
             write_groupseq(melody)
         else:
             raise SystemExit
-    return tools.flatten_sequence(seq)
+    grouped_seq = write_seq(seq)
+    return tools.flatten_sequence(grouped_seq)
 
 
-functions = {'1': write_seq, '2': write_mapseq, '3': write_sparseseq,
-             '4': write_chordseq, '5': write_groupseq}
+sequence_types = {'1': write_seq, '2': write_mapseq, '3': write_sparseseq,
+                 '4': write_chordseq, '5': write_groupseq}
 
 
 def main():
-    melody = get_melody()
+    input_method = get_input_method()
+    input_sequence = get_input_sequence(input_method)
     help_msg = ' '.join(('1 : Basic Sequence,',
                          '2 : Mapped Sequence,',
                          '3 : Sparse Sequence,',
                          '4 : Chord Sequence,',
                          '5 : Grouped Sequence.'))
     seq_type = str(input('Sequence Type {}: '.format(help_msg)))
-    sequence = functions.get(seq_type)
+    sequence = sequence_types.get(seq_type)
     if sequence:
-    	output_tracks = []
-    	for track in melody:
-        	seq = sequence(track)
-        	output_tracks.append(seq)
+        output_tracks = []
+        for track in input_sequence:
+            seq = sequence(track)
+            output_tracks.append(seq)
     else:
         print('Invalid Sequence Type. Must be 1, 2, 3, 4 or 5. Look at Help.')
         raise SystemExit
     output_name = get_output_name()
-    write_midifile(output_name, output_tracks)
+    output_type = input_method == read_rhythms
+    write_midifile(output_name, output_tracks, output_type)
 
-main()
+if __name__== '__main__':
+    main()
